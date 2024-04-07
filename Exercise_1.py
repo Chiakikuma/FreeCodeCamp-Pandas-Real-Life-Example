8305fa8f8b1e601f368b63bcfcb9c2d7b7db56f8
Chiakikuma <102116133+Chiakikuma@users.noreply.github.com>
2024-04-07 13:47:51 +0000

    tried out most exercises from Exercise_1

---
+++ Exercise_1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+
+sales = pd.read_csv(
+    'data/sales_data.csv',
+    parse_dates=['Date'])
+
+sales.head()
